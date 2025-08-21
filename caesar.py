eee1aaee7aac4fae2ed0938afd60bd1c72ad18ac
ned777 <nmdcnn@gmail.com>
2025-08-21 03:45:40 -0700

    Decrypted but all in lowercase

--- caesar.py
+++ caesar.py
@@ -1,4 +1,14 @@
-encrypted_text=''
+encrypted_text='khoor cdlud'
+shift=3
 
 def decrypt():
-    
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    text=''
+
+    for char in encrypted_text.lower():
+        index=alphabet.find(char)
+        new_index=(index - shift) % len(alphabet)
+        text+=alphabet[new_index]
+    print(text)
+
+decrypt()
